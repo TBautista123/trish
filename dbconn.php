a1a7e8a27eedc80d3d290ed9351a866f10b2d340
TBautista123 <167744438+TBautista123@users.noreply.github.com>
2024-05-12 12:26:07 +0800

    asdas

--- dbconn.php
+++ dbconn.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect("mysql.hostinger.com","u240496009_trish","Trish_12345","u
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
-echo "Connection successfully";
+//echo "Connection successfully";
 
 ?>
 
