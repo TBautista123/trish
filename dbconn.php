983108d6c73bf342e87f29629d8a6cc7b0f92efd
TBautista123 <167744438+TBautista123@users.noreply.github.com>
2024-05-12 12:10:58 +0800

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,5 @@
 <?php
 
-$conn = mysqli_connect("mysql.hostinger.com","u240496009_user","Trish_12345","","u240496009_trish");
+$conn = mysqli_connect("mysql.hostinger.com","u240496009_trish","Trish_12345","u240496009_trish");
 
 ?>
-dasdsadash
